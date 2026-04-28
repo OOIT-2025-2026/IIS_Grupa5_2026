f16c9be6b77da7dc3f8962902af860537e17224c
NadjaKljestanovic <nadja.kljestanovic@gmail.com>
2026-04-28 10:41:48 +0200

    Domaci - vezbe 4 - klasa Circle

--- InitialProject/src/geometry/Circle.java
+++ InitialProject/src/geometry/Circle.java
@@ -5,12 +5,42 @@ public class Circle {
 	private int radius;
 	private boolean selected;
 	
+	public Circle() {
+
+
+	}
+	
+	public Circle(Point center, int radius) {
+		this.center = center;
+		this.radius = radius;
+	}
+	
+	public Circle(Point center, int radius, boolean selected) {
+		this(center, radius);
+		this.selected = selected;
+	}
+	
 	public double area() {
 		return radius * radius * Math.PI;
 	}
 	
 	public double circumference() {
 		return 2 * radius * Math.PI;
+	}
+	
+	public String toString() {
+		return "Center: " + center + ", radius = " + radius;
+	}
+	
+	public boolean equals(Object obj) {
+		if (obj instanceof Circle) {
+			Circle secondCircle = (Circle) obj;
+			if (this.center.equals(secondCircle.center) && this.radius == secondCircle.radius)
+				return true;
+			else
+				return false;
+		} else
+			return false;
 	}
 
 	public Point getCenter() {
